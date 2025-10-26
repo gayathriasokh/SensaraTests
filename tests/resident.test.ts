839b63edf7b9578ef167778634efcbbedd52f9af
gayathriasokh <gayathri.asokh@sensara.eu>
2025-10-26 13:46:56 +0100

    add common browser open functionality

--- tests/resident.test.ts
+++ tests/resident.test.ts
@@ -2,23 +2,31 @@ import test, {expect} from "../src/utils/fixtures";
 import ENV from "../src/utils/env";
 import LoginPage from "../src/pages/login.page";
 import ResidentPage from "../src/pages/resident.page"
-import AlarmOverviewPage from "../src/pages/alarmOverview.page";
-import data from "../testData/data.json";
+import { Page } from "@playwright/test";
 
-test("Validating the login functionality", async({page,loginPage,residentPage, base}) => {
+let page: Page;
+let loginPage: LoginPage;
+
+test.describe('Resident Page Tests', () => {
+
+test.beforeAll(async ({ browser }) => {
+
+    page = await browser.newPage();
+    loginPage = new LoginPage(page);
+     await page.goto(ENV.BASE_URL);
+  });
+
+test.afterAll(async () => {
+    await page.close();
+  });
+  
+test("Validating the login functionality", async() => {
    
-    await page.goto(ENV.BASE_URL);
     await loginPage.enterUserName(ENV.USERNAME);
     await loginPage.enterPassword(ENV.PASSWORD);
     await loginPage.clickSignInBtn();
     await page.screenshot({ path: 'ResidentPage.png', fullPage: true });
-    await residentPage.clickResidentLink();
-
-
 })
 
-// test("Validating the resident page", async({page,residentPage, base}) => {
-   
-//     await residentPage.clickResidentLink();
 
-// })
+}) 
